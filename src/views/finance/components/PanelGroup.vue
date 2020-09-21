c28abb7c226e29e6a06605232fef809f5668e000
laiqiaofeng <2425138652@qq.com>
2020-09-21 18:05:27 +0800

    更改icon

--- src/views/finance/components/PanelGroup.vue
+++ src/views/finance/components/PanelGroup.vue
@@ -9,7 +9,7 @@
     <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
       <div class="card-panel">
         <div class="card-panel-icon-wrapper icon-people">
-          <svg-icon icon-class="income" class-name="card-panel-icon" />
+          <svg-icon icon-class="withdrawal" class-name="card-panel-icon" />
         </div>
         <div class="card-panel-description">
           <div class="card-panel-text">
@@ -22,7 +22,7 @@
     <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
       <div class="card-panel">
         <div class="card-panel-icon-wrapper icon-pattern">
-          <svg-icon icon-class="expend" class-name="card-panel-icon" />
+          <svg-icon icon-class="income-new" class-name="card-panel-icon" />
         </div>
         <div class="card-panel-description">
           <div class="card-panel-text">
